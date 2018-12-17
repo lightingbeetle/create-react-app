b61856f9291edb5f1834144421674ab75f9b9927
Dusan Tuzinsky <dusan.tuzinsky@gmail.com>
2018-12-17 11:13:31 +0100

    shape in a code block preformatted

--- packages/styleguide/src/components/ComponentDocs/utils.js
+++ packages/styleguide/src/components/ComponentDocs/utils.js
@@ -48,7 +48,7 @@ export async function getComponentInfo(path, getCode) {
 
           return await getComponentInfo(componentPath, getCode);
         })
-      )),
+      ))
     ];
   }
 
@@ -64,9 +64,14 @@ function printEnums(values) {
 function printShape(values) {
   const keyValues = Object.keys(values)
     .map(name => name + ': ' + getType(values[name]))
-    .join(', ');
+    .join(', \r\n');
+
   return codeBlock`
-    { ${keyValues} }
+    \`\`\`js
+    { 
+      ${keyValues.replace(/```js\n/g, '').replace(/\s+}\n```/g, '\n}')}
+    }
+    \`\`\`
   `;
 }
 
@@ -122,8 +127,8 @@ function getPropsData(props = {}) {
         prop: `${props[prop].required ? '* ' : ''}${prop}`,
         type: getType(props[prop].type),
         default: props[prop].defaultValue && props[prop].defaultValue.value,
-        description: props[prop].description,
-      },
+        description: props[prop].description
+      }
     ],
     []
   );
